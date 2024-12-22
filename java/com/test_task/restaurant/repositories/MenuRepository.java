6a9fb124775909a0508f4d052f59e2563c0aaabf
Alex <46602606+IKdotShark@users.noreply.github.com>
2024-12-22 21:44:02 +0500

    Delete java/com/test_task/restaurant directory

--- java/com/test_task/restaurant/repositories/MenuRepository.java
+++
@@ -1,7 +0,0 @@
-package com.test_task.restaurant.repositories;
-
-import com.test_task.restaurant.models.Menu;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuRepository extends JpaRepository<Menu, Long> {
-}
