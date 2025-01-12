e9952c349af1ff91bfd877a341a999f498360e66
Язов Михаил <100093644+KuroshiXD@users.noreply.github.com>
2025-01-12 22:52:17 +0500

    Delete Restaurant/java/com/test_task/restaurant directory

--- Restaurant/java/com/test_task/restaurant/repositories/DesertRepository.java
+++
@@ -1,7 +0,0 @@
-package com.test_task.restaurant.repositories;
-
-import com.test_task.restaurant.models.Desert;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DesertRepository extends JpaRepository<Desert, Long> {
-}
--- Restaurant/java/com/test_task/restaurant/repositories/StaffRepository.java
+++
@@ -1,7 +0,0 @@
-package com.test_task.restaurant.repositories;
-
-import com.test_task.restaurant.models.Staff;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StaffRepository extends JpaRepository<Staff, Long> {
-}
