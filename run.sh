a3340fc7cb3ea7b0977e2e0f8469d6336c2f9fc1
Язов Михаил <100093644+KuroshiXD@users.noreply.github.com>
2025-01-18 00:31:54 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 echo "Starting backend..."
 cd Restaurant
-./mvnw spring-boot:run &
+./mvnw spring-boot:run -Dspring-boot.run.arguments=--server.port=8088 &
 
 echo "Starting frontend..."
 cd ../frontend/restaurant
